7f1bf5499fb9e71e6a2dcb0d64992e3b863d6a33
Jvson03 <jvsonlamaj@gmail.com>
2022-11-15 21:26:44 +0100

    autorole

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,10 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.GuildMemberAdd,
+	execute(member) {
+		const role = member.guild.roles.cache.get('962309852972064788');
+		// Add the role!
+		member.role.add(role).catch(console.error);
+	},
+};
